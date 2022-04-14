9043007639cf2feabd63870ec4560bcfa15af9fe
alion <smirnoviksa@gmail.com>
2022-04-14 20:24:32 +0500

    Add baseic GET controller

---
+++ TestTaskApiPrototype2/Controllers/ClientsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestTaskApiPrototype2.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic; //TODO: SORR USINGS DIRECTIVES
+
+
+namespace TestTaskApiPrototype2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClientsController
+    {
+        private readonly ApplicationContext _context;
+
+        public ClientsController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Client>>> GetClients()
+        {
+            return await _context.Clients.ToListAsync();
+        }
+
+    }
+}
