0325565f5273bf0115933930a8f9d9dc0ed67042
alion <smirnoviksa@gmail.com>
2022-04-14 19:19:26 +0500

    Add DocumentPassport model

---
+++ TestTaskApiPrototype2/Models/DocumentPassport.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace TestTaskApiPrototype2.Models
+{
+    public class DocumentPassport
+    {
+        [Key]
+        public int DocumentPassportId { get; set; }
+
+
+        [DataMember(Name = "id")]
+        public Guid Id { get; set; }
+
+        [DataMember(Name = "series")]
+        public string Series { get; set; }
+
+        [DataMember(Name = "number")]
+        public string Number { get; set; }
+
+        [DataMember(Name = "giver")]
+        public string Giver { get; set; }
+
+        [DataMember(Name = "dateIssued")]
+        public DateTime DateIssued { get; set; }
+
+        [DataMember(Name = "birthPlace")]
+        public string BirthPlace { get; set; }
+
+        [DataMember(Name = "createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [DataMember(Name = "updatedAt")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
