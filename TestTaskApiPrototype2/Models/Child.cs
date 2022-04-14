12cdf7b5f5c70c345b0ff23e0477cc0e3eaf44ee
alion <smirnoviksa@gmail.com>
2022-04-14 17:53:02 +0500

    Add Child models

---
+++ TestTaskApiPrototype2/Models/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace TestTaskApiPrototype2.Models
+{
+    public class Child
+    {
+        [Key] //стоит ли явно указывать?
+        public int ChildId { get; set; }
+
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "surname")]
+        public string Surname { get; set; }
+
+        [DataMember(Name = "patronymic")]
+        public string Patronymic { get; set; }
+
+        [DataMember(Name = "dob")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
